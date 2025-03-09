29bd30502e3e6592eb79bb8a24e4736bcb654708
efedincer <dincer.efe@gmail.com>
2025-03-09 11:35:06 +0300

    Add project files.

---
+++ OnlyBootstrap5TemplateAndDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlyBootstrap5TemplateAndDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
